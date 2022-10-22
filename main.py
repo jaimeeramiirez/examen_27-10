e2def421fd588f6aaa604503bf332d85dbed53e6
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-22 12:15:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+print("\n\n")
+
+lista = [17, 24, 2, 13, 5, 4, 6, 1, 89, 0, 3, 7, 55, 25, 30, 8, 68, 156]
+
+
+def separar(lista):
+  
+    pares = []
+    impares = []
+
+    for n in lista:
+
+        if n%2 == 0:
+
+            pares.append(n)
+        else:
+            impares.append(n)
+    
+    pares.sort()
+    impares.sort()
+  
+    return pares, impares
+  
+  
+
+
+pares, impares= separar(lista)
+print('Pares: ', pares)
+print('Impares: ', impares)
