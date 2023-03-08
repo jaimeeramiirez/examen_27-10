20f69361fb938fb50ea354354e9f5d95e87ed9aa
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2023-03-08 13:26:54 +0000

    he recuperado el ejercicio 5

--- ejercicio5.py
+++ ejercicio5.py
@@ -5,22 +5,20 @@
 
 print("\n\n")
 
-num= float(input("INGRESE UN NÚMERO DE 4 CIFRAS: "))
+num = float(input("INGRESE UN NÚMERO DE 4 CIFRAS: "))
 
-mil= (num - (num%1000)) / 1000
-resto= num%1000
-centena= (resto-(resto%100)) / 100
-resto = num%100
-decena= (resto-(resto%10)) / 10
+mil = (num - (num % 1000)) / 1000 * 1000
+resto = num % 1000
+centena = (resto - (resto % 100)) / 100
+resto = num % 100
+decena = (resto - (resto % 10)) / 10
 unidad = resto % 10
 print("\n")
 
-print("UNIDADES:",'%04d' % unidad)
+print("UNIDADES:", '%04d' % unidad)
 print("DECENAS:", '%03d' % decena)
-print("CENTENAS:",'%02d' % centena)
-print("MIL:",'%01d' % mil)
+print("CENTENAS:", '%02d' % centena)
+print("MIL:", '%01d' % mil)
 
-
-numero=str(12).ljust(4, '0')
-
-print(numero)
+x = str(12).ljust(4, "0")
+print(x)
